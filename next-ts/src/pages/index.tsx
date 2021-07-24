5a4be8048300674c65fea86195ffd1f71202c242
leobar37 <leonardito3458@gmail.com>
2021-07-24 16:24:19 -0500

    (chore) init base

--- next-ts/src/pages/index.tsx
+++ next-ts/src/pages/index.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { Layout, List, Typography } from "antd";
-import { PlaceHolderApi } from "services/PlaceHolder";
-import {
-  GetServerSideProps,
-  GetStaticProps,
-  InferGetStaticPropsType,
-} from "next";
-import { Ipost } from "models/Post";
-import Link from "next/link";
-import { Row, Card, Col } from "antd";
+
 const { Header, Content, Footer } = Layout;
 
 function Index() {
